7e250f61a771d2ae6bb365bc9dd8225db1800e9a
trentglobal-sg <paul.chor@trentglobal.edu.sg>
2026-02-07 06:33:57 +0000

    Added create

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const app = express();
 app.set('view engine', 'ejs');  // tell Express that we are using EJS as our template engine
 app.use(express.static('public'));
 
-const dbConfig= {
+// handle forms via POST
+app.use(express.urlencoded({
+  extended: true
+}))
+
+const dbConfig = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_NAME,
@@ -18,15 +23,15 @@ const dbConfig= {
 // create a new connection pool
 const dbConnection = mysql2.createPool(dbConfig);
 
-app.get('/test', async function(req,res){
-    const luckyNumber = Math.floor(Math.random() * 10000 + 1);
-   res.render('test', {
-     "websiteTitle":"Puppy Love",
-     "luckyNumber":luckyNumber
-   });
+app.get('/test', async function (req, res) {
+  const luckyNumber = Math.floor(Math.random() * 10000 + 1);
+  res.render('test', {
+    "websiteTitle": "Puppy Love",
+    "luckyNumber": luckyNumber
+  });
 });
 
-app.get('/', async function(req,res){
+app.get('/', async function (req, res) {
   const sql = `SELECT * FROM food_entries`;
   // dbConnection.execute will return with an array of two elements:
   // index 0: row data (we want this)
@@ -34,13 +39,31 @@ app.get('/', async function(req,res){
   // we can use array destructuring to assign elements from an array
   // into a variable by the order of the variable in the array 
   // on the left hand size
-  const [rows]= await dbConnection.execute(sql);
-  
+  const [rows] = await dbConnection.execute(sql);
+
   res.render("index", {
     "foodEntries": rows
   })
+});
+
+
+
+app.get('/food-entry/create', async function (req, res) {
+  res.render('create-food-entry');
 })
 
-app.listen(3000, function(){
-    console.log("Server started")
+app.post('/food-entry/create', async function(req,res){
+  console.log(req.body);
+ // create a prepared query, aka parameterized query
+ const sql = `INSERT INTO food_entries (dateTime, foodName, calories, meal, tags, servingSize, unit)
+            VALUES (?, ?, ?, ?, ?, ?, ?);`
+ const bindings = [req.body.dateTime, req.body.foodName, req.body.calories, 
+  req.body.meal, JSON.stringify(req.body.tags), req.body.servingSize, req.body.unit];
+ console.log(bindings);
+ const results = await dbConnection.execute(sql, bindings);
+ res.redirect('/');
+})
+
+app.listen(3000, function () {
+  console.log("Server started")
 })
---
+++ test_queries.sql
@@ -0,0 +1,2 @@
+INSERT INTO food_entries (dateTime, foodName, calories, meal, tags, servingSize, unit)
+            VALUES (NOW(), "Chicken Rice", 700, "lunch", "[\"glutten-free\"]", 1, "pounds")
